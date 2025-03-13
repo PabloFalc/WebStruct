9aa5ef97096663422e381682c9a6d967e893c9d8
pablo  martins <pablo.martins@icev.lab>
2025-03-13 17:33:51 -0300

    filas

---
+++ projects/src/main/java/com/projects/filas/Fila.java
@@ -0,0 +1,70 @@
+package com.projects.filas;
+
+public class Fila {
+
+    public Node head;
+    public Node tail;
+
+    public Fila(Node tail, Node head){
+        this.head = null;
+        this.tail = null;
+
+    }
+
+
+
+    public boolean Queue(int element){
+
+
+        if(this.head == null || this.tail == null){
+            Node node = new Node(element, null);
+
+            this.tail = node;
+            this.head = node;
+            System.out.println("lista vazia, elemento add");
+           return true;
+        }
+        System.out.println("Lista tem mais de um elemento");
+        Node node = new Node(element,null);
+
+        System.out.println(node.value);
+
+        node.next = this.head;
+        this.head = node;
+
+
+
+        return true;
+
+    }
+
+
+    public Node Dequeue(){
+
+        if(this.head == null || this.tail == null){
+            throw new NullPointerException("operção com fila nula");
+        }
+        if(this.head == this.tail){
+            Node element = this.head;
+
+            this.head = null;
+            this.tail =null;
+
+            System.out.println("apenas um elemento, na lista, e foi removido");
+            return element;
+        }
+        System.out.println("tem mais de 1 elemento na fila");
+        Node element = this.tail;
+        Node actual = this.head;
+
+        while (actual.next != element){
+            actual = actual.next;
+        }
+
+        actual.next = null;
+        this.tail = actual;
+
+        System.out.println("Seu elemento foi: "+ element.value);
+        return element;
+    }
+}
---
+++ projects/src/main/java/com/projects/filas/teste.java
@@ -0,0 +1,36 @@
+package com.projects.filas;
+
+public class teste {
+
+    public static void main( String[] asdasdsadsa){
+
+
+
+
+
+        Fila fila = new Fila(null, null);
+
+        fila.Queue(10);
+        fila.Queue(20);
+        fila.Queue(40);
+        fila.Queue(30);
+        fila.Queue(60);
+        fila.Queue(70);
+
+        System.out.println("----------------");
+        fila.Dequeue();
+        fila.Dequeue();
+        fila.Dequeue();
+        fila.Dequeue();
+        fila.Dequeue();
+        fila.Dequeue();
+        fila.Dequeue();
+
+
+
+
+
+    }
+
+
+}
