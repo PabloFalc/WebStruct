625664975633245a7ad92d72ae36ad3ef06f0468
PabloFalc <pablopatricio640@gamil.com>
2025-03-19 21:52:58 -0300

    ajustando um pouco o remover

--- DataStruct/tree/Tree.java
+++ DataStruct/tree/Tree.java
@@ -22,6 +22,7 @@ public class Tree<Type extends Comparable<Type>> {
         Node<Type> node = new Node<>(element); 
         if(this.root == null){
             this.root = node;
+            return;
         }
         Node<Type> current = this.root;
 
@@ -63,10 +64,92 @@ public class Tree<Type extends Comparable<Type>> {
 
     
     public Type delete(Type element){
-        
+        /*
+        casos:
+
+        não tem filho
+        tem filho so de um lado
+        tem filho dos dois lados
+
+
+
+
+         */
         if(this.root == null){
             throw new RuntimeException("A árvore está nula");
         }
+
+        Node<Type> current = this.root;
+        Node<Type> currentParent = null;
+
+        while (current!=null) {
+            // break pra parar a busca
+            if(current.value.equals(element)){
+              break;  
+            }
+            // caso o elemento seja menor que o atual
+            else if(current.value.compareTo(element) < 1){
+                currentParent = current;
+                current = current.left;
+            }
+            // caso o elemento seja maior que o atual;
+            else{
+                currentParent = current;
+                current = current.right;
+            }
+            
+        }
+        // o atual tem  fihlos em ambos os lados
+        if (current.left != null && current.right != null) {
+            
+        }
+        // o atual tem filhos a esquerda
+        else if(current.left != null){
+
+        }
+        // o atual tem filhos a direita
+        else if (current.right != null){
+
+            Node<Type> replacement = current.right;
+            Node<Type> replacementParent = current;
+
+
+            while (replacement.left != null) {
+                replacementParent = replacement;
+                replacement = replacement.left;  
+            }
+
+
+            if(replacement.value.compareTo(replacementParent.value) < 1){
+                replacementParent.left = null;
+            }
+            else{
+                replacement.right = null;
+            }
+            
+            if (currentParent.value.compareTo(current.value) < 1) {
+                currentParent.left = replacement;
+            }
+            else{
+                currentParent.right = replacement;
+            }
+            
+        }
+        // o atual não tem filhos
+        else{
+            if(currentParent.value.compareTo(element) < 1){
+                currentParent.left = null;
+                return current.value;
+            }
+            else{
+                currentParent.right = null;
+                return current.value;
+            }
+
+        }
+
+
+
         return element;
     }
   
@@ -107,32 +190,44 @@ public class Tree<Type extends Comparable<Type>> {
     }
     
 
-    private void posOrder(Node <Type> current, Stack<Type> nodeStack){
+    private void postOrder(Node <Type> current, Stack<Type> nodeStack){
 
         if(current == null){
             return;
         }
-        posOrder(current.left, nodeStack);
-        posOrder(current.right, nodeStack);
+        postOrder(current.left, nodeStack);
+        postOrder(current.right, nodeStack);
         nodeStack.push(current.value);
     }
 
-    public Stack<Type> getPosOrderStack(){
+    public Stack<Type> getPostOrderStack(){
         Stack<Type> nodeStack = new Stack<>();
-        posOrder(this.root, nodeStack);
+        postOrder(this.root, nodeStack);
         nodeStack.printStack();
         return nodeStack;
     }
     
 
 
-    public int countNodes(){
-       return 0;
-    }
-
     public int height(Type element){
-        
-        return 0;
+        Node<Type> current = this.root;
+        int count = 0 ;
+        while (current != null) {
+            if(current.value.compareTo(element) == 0){
+                break;
+            }
+
+            if (current.value.compareTo(element) < 1) {
+                current = current.left;
+                count++;
+            }
+            else{
+                current = current.right;
+                count++;
+            }
+
+        }
+        return count;
     }
 
     public Type seach(Type element){
@@ -162,16 +257,24 @@ public class Tree<Type extends Comparable<Type>> {
     public static void main(String[] args) {
         Tree<Integer> tree = new Tree<>();
 
+        // java DataStruct/tree/Tree.java 
         tree.insert(20);
         tree.insert(15);
         tree.insert(18);
-        // tree.insert(10);
-        // tree.insert(7);
-        // tree.insert(11);
-        // tree.insert(30);
-        // tree.insert(25);
-        // tree.insert(31);
+        tree.insert(10);
+        tree.insert(7);
+        tree.insert(11);
+        tree.insert(30);
+        tree.insert(25);
+        tree.insert(31);
+        System.out.println("------IN---------");
         tree.getInOrderStack();
+        System.out.println("-------Pre--------");
+        tree.getPreOrderStack();
+        System.out.println("-------Post--------");
+        tree.getPostOrderStack();
+
+        System.out.println(tree.height(11));
     }
 
 }
